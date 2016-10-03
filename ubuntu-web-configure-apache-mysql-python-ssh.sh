af899087a399a00c86e63a3fbb870ece5144b3d8
Invent XP <inventxp@gmail.com>
2016-10-03 23:21:43 +0530

    Update ubuntu-web-configure-apache-mysql-python-ssh.sh

--- ubuntu-web-configure-apache-mysql-python-ssh.sh
+++ ubuntu-web-configure-apache-mysql-python-ssh.sh
@@ -1,1 +1,36 @@
-ufw allow 80
+#!/bin/bash
+
+# ###########################################################
+# Setup UFW Firewall
+# ###########################################################
+sudo ufw allow 80
+sudo ufw allow 443
+sudo ufw allow 3273
+sudo ufw enable
+
+# ###########################################################
+# Configuring MySQL Database
+# ###########################################################
+
+sudo mysql_install_db
+
+VAR=$(expect -c '
+spawn apt-get -y install mysql-server
+expect "New password for the MySQL \"root\" user:"
+send "PasswordHere\r"
+expect "Repeat password for the MySQL \"root\" user:"
+send "PasswordHere\r"
+expect eof
+')
+
+echo "$VAR"
+
+sudo /usr/bin/mysql_secure_installation
+
+
+# ###########################################################
+# Service Restart
+# ###########################################################
+sudo service ssh restart
+sudo service apache2 restart
+
